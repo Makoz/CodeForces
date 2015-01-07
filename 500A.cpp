90c937be26d47d777df7216c4c2ea20accb6ffc3
Makoz <Makoz@users.noreply.github.com>
2015-01-06 22:27:57 -0800

    Create 500A.cpp

---
+++ 500A.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <map>
+#include <iostream>
+// 500A - http://codeforces.com/problemset/problem/500/A
+using namespace std;
+int N, idx, A;
+
+
+// next1 stores the next index and next2 stores how much time it takes to get from next1[i]->next1[j]
+
+
+
+
+int main(){
+
+
+	std::cin >> N;
+	cin >> idx;
+	int currIdx =1;
+	int b = 1;
+
+	while(N--){
+		cin >> A;
+		currIdx += A;
+		b++;
+		if(currIdx > b){
+			while(currIdx > b){
+				cin >> A;
+				b++;
+			}
+		}	
+		if(currIdx >= idx) break;
+	}
+	if(currIdx == idx){
+		cout << "YES" << endl;
+	}
+	else{
+		cout << "NO" << endl;
+	}
+
+
+return 0;
+}
