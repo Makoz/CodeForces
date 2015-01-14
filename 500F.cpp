493e59cb459a62e3b2996f8f4ec578708d5c54cd
Makoz <Makoz@users.noreply.github.com>
2015-01-13 16:00:34 -0800

    Update 500F.cpp
    Still fails at test 7 with more efficient search

--- 500F.cpp
+++ 500F.cpp
@@ -1,3 +1,9 @@
+// Codeforces.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
 #include <string>
 #include <map>
 #include <iostream>
@@ -7,11 +13,9 @@
 #include <limits>
 #include <math.h>
 // http://codeforces.com/problemset/problem/500/F
-// Fails on Test 7, believe sorting the array takes too long as no output was generated as seen here:
-// http://codeforces.com/contest/500/submission/9362706
-// Could implement pseudo-binary search first to see if run time improves
-int N,P, Q;
-int A,B,C,D;
+// still fails at Time 7 with more efficient search
+int N, P, Q;
+int A, B, C, D;
 struct item{
 	int cost;
 	int happy;
@@ -20,80 +24,117 @@ struct item{
 using namespace std;
 item p[4010];
 
-bool sortOrder(item i1, item i2){ return i1.time < i2.time;}
+bool sortOrder(item i1, item i2){ return i1.time < i2.time; }
 
 int returnMaxCombo(int startIdx, int endIdx, int budget){
-	
-	if(budget == 0) return 0;
-	if(startIdx > endIdx){
+
+	if (budget == 0) return 0;
+	if (startIdx > endIdx){
 		return 0;
 	}
 	int a = 0;
 	int b = budget;
-	if(p[startIdx].cost <= budget){
+	if (p[startIdx].cost <= budget){
 		a = p[startIdx].happy;
-		b = budget-p[startIdx].cost;
-	
+		b = budget - p[startIdx].cost;
+
 	}
-	if(b == budget){
-		return returnMaxCombo((startIdx+1), endIdx, budget);
+	if (b == budget){
+		return returnMaxCombo((startIdx + 1), endIdx, budget);
 	}
-	
-	return max(returnMaxCombo((startIdx+1), endIdx, budget), (returnMaxCombo((startIdx+1), endIdx, b)+a));
+
+	return max(returnMaxCombo((startIdx + 1), endIdx, budget), (returnMaxCombo((startIdx + 1), endIdx, b) + a));
 }
+
+// We Want to find the maximum index i such that P[i] <= A and P[i+1] > A
+
+int findMaxIndexHelper(int A, int maxIdx, int minCap, int pivot){
+	if (maxIdx < minCap) return -1;
+	if (p[pivot].time <= A && p[pivot + 1].time > A) return pivot;
+	if (p[pivot].time > A) return findMaxIndexHelper(A, (pivot - 1), minCap, ((pivot - 1 + minCap) / 2));
+	// if (p[pivot].time < A) 
+	return findMaxIndexHelper(A, maxIdx, (pivot + 1), ((maxIdx + pivot + 1) / 2));
+
+}
+
+int findMaxIndex(int A, int maxIdx, int minCap){
+	// cout << "A is: " << A << ", pmaxIdx.time " << p[maxIdx].time << " P is:" << P << endl;
+	if (p[maxIdx].time < A && ((p[maxIdx].time + P-1) < A)) return -1;
+	if (p[maxIdx].time <= A) return maxIdx;
+	if (maxIdx == minCap) return -1;
+	int pivot = (maxIdx - 1 + minCap) / 2;
+	return findMaxIndexHelper(A, maxIdx-1, minCap, pivot);
+}
+
+
+int findMinIndexHelper(int A, int maxIdx, int minCap, int pivot){
+	if (maxIdx < minCap) return N;
+	if (p[pivot].time > A) return findMinIndexHelper(A, (pivot - 1), minCap, (pivot + minCap) / 2);
+	if (p[pivot].time <= A && p[pivot].time + P-1 >= A && p[pivot - 1].time + P-1 < A) return pivot;
+	if (p[pivot].time < A  && p[pivot].time + P - 1 < A) return findMinIndexHelper(A, maxIdx, pivot+1, 1+(maxIdx + pivot) / 2);
+	return findMinIndexHelper(A, pivot, minCap, ((pivot + minCap) / 2));
+}
+
+int findMinIndex(int A, int maxIdx, int minCap){
+	if (p[minCap].time > A || p[maxIdx].time+P-1 < A) return N;
+	if (p[minCap].time <= A && p[minCap].time + P-1 >= A) return minCap;
+	int pivot = (maxIdx + minCap + 1) / 2;
+	return findMinIndexHelper(A, maxIdx, minCap, pivot);
+}
+
+
 
 
 int main(){
 
- cin >> N >> P;
- int idx =0;
- int L=N;
- while(L--){
-	item temp;
-	cin >> A;
-	cin >> B;
-	cin >> D;
-	temp.cost = A;
-	temp.happy = B;
-	temp.time = D;
+	cin >> N >> P;
+	int idx = 0;
+	int L = N;
+	while (L--){
+		item temp;
+		cin >> A;
+		cin >> B;
+		cin >> D;
+		temp.cost = A;
+		temp.happy = B;
+		temp.time = D;
 
-	p[idx] = temp;
-	 idx++;
- }
+		p[idx] = temp;
+		idx++;
+	}
 
- sort(p, p+N, sortOrder);
-cin >> Q;
+	sort(p, p + N, sortOrder);
+	/*
+	for(int i =0 ; i<N; i++){
+	cout << p[i].cost << " " << p[i].happy << " " << p[i].time << endl;
+	}
+	*/
+	cin >> Q;
+	while (Q--){
+		cin >> A >> B;
+		// A is visittime, B is budget
+		
+		int minIndex = findMinIndex(A, (N - 1), 0);
+		// adjust maxIndex
+		int maxIndex = findMaxIndex(A,(N-1), 0);
+		/*
+		int desm1 = N - 1;
+		while (p[desm1].time > A){
+			desm1--;
+		}
+		
+		int desm2 = 0;
+		while ((p[desm2].time + P - 1) < A){
+			desm2++;
+		}
+		
+		cout << maxIndex << " " << minIndex << " desired: " << desm1 << " " << desm2 <<endl;
+		*/
+		int util = 0;
+		util += returnMaxCombo(minIndex, maxIndex, B);
+		cout << util << endl;
+	}
 
- while(Q--){
-cin >> A >> B;
-// A is visittime, B is budget
-int maxIndex = N-1;
-int minIndex = 0;
-int tMax = N-1;
-int tMin = 0;
-// Lazy implementation, of finding proper max/min indexes
-// Adds some bottleneck
-// TODO: reimplement proper pseudo-binary to improve find time
-
-while(p[maxIndex].time > A && maxIndex > 0){
-	maxIndex = maxIndex/2 - 1;
-}
-while(maxIndex != N-1 && p[maxIndex+1].time <= A){
-	maxIndex++;
-}
-if(p[maxIndex].time > A){
-	maxIndex = -1;
+	return 0;
 }
 
-while(((p[minIndex].time + P-1) < A && minIndex <(N-1))){
-	minIndex = ((N-1)+minIndex)/2 +1;
-}
-while(minIndex > 0 && (p[minIndex-1].time + P-1) >= A){
-	minIndex--;
-}
-int util = 0;
-util += returnMaxCombo(minIndex,maxIndex, B);
-cout << util << endl;
-}
-return 0;
-}
